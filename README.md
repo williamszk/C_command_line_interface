0eb3e849314b9537bc63a3ada7a203aa25445bc4
williamszk <wllmszk@gmail.com>
2020-10-19 19:07:21 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # C_command_line_interface
+
+## Objective
+
+Build a command line interface using C.
---
+++ main.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    // 2-d arrays, create 2 square brackets
+    int nums[][] = {
+        {1,2},
+        {3,4}
+    };
+
+
+    for (i = 0; i <=1; i++){
+        for (j = 0; j <= 1, j++){
+            printf("%d, %d\n",);        
+        }
+    }
+    
+
+
+    return 0;
+}
