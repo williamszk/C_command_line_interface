bae253f58455b1ec254d77b7c4d486d45784152b
williamszk <wllmszk@gmail.com>
2020-10-19 19:39:02 -0300

    update

--- main.c
+++ main.c
@@ -4,14 +4,15 @@
 
 int main(){
 
-    int command;
+    char command;
     int state = 1;
 
+    printf("+-----------------------------------------+\n| This is a Command Line Interface.\n| This is a project to learn C programming\n|\n| William Suzuki\n| oct - 2020\n+-----------------------------------------+\n\n\n");
 
     while (state == 1){
-        printf("Type a numeric command:");
-        scanf("%d", &command);
-
+        printf(">");
+        scanf("%c", &command);
+        printf("%c",command);
     }
 
 
